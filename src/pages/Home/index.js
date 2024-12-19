e0fb6f8abbbb4cdd75161049ec0d2ede56f65aba
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2024-12-19 01:11:09 -0300

    Listando Filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import './home.css';
 function Home() {
     // Declara um estado chamado `filmes` para armazenar os dados recebidos da API.
     // `setFilmes` é a função usada para atualizar o estado.
@@ -22,10 +24,10 @@ function Home() {
             });
 
             // Exibe os resultados recebidos da API no console do navegador.
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0,10));
+            setFilmes((response.data.results.slice(0,10)));
 
-            // Atualiza o estado `filmes` com os dados recebidos da API.
-            setFilmes(response.data.results);
+          
         }
 
         // Chama a função que carrega os filmes.
@@ -36,8 +38,19 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            <h1>PAGINA - HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=>{
+                      return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                      )
+                })}
+
+            </div>
         </div>
     )
 }
