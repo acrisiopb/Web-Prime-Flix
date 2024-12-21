67fe7ac38483409fa4f9569b18963dfc490a5b18
Acrísio_Cruz <81418982+acrisiopb@users.noreply.github.com>
2024-12-20 22:08:39 -0300

    Excluindo filme

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,19 @@ function Favoritos(){
 
     },[]);
 
+    function excluirFilme (id){
+       let filtroFilmes = filmes.filter((filme)=>{
+        return(filme.id !== id);
+       })
+
+       setFilmes(filtroFilmes);
+       localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+    }
+
     return(
      <div className="meus-filmes">
         <h1>Meus Filmes</h1>
+        {filmes.length === 0 && <span>Você não possui nenhum filme salvo</span>}
         <ul>
             {filmes.map((filme) =>{
              return(
@@ -20,7 +30,7 @@ function Favoritos(){
                     <span>{filme.title}</span>
                     <div>
                      <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
-                     <button>Excluir</button>
+                     <button onClick={() => excluirFilme(filme.id)}>Excluir</button>
                     </div>
                 </li>
              )
